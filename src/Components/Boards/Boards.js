f1c59e6e491fee58f501b2c400b165cd996d0ae1
Jon M Hobbs <jon.hobbs@minnykid.com>
2022-05-26 19:23:40 -0500

    Format cleanup

--- src/Components/Boards/Boards.js
+++ src/Components/Boards/Boards.js
@@ -11,17 +11,16 @@ const Boards = ({boards, uploadButton}) => {
   const { currentProject, importBoards } = useProjects()
   const { fetchViaApi } = useFetch()
 
-
-
   const sheetBoards = boards ? boards.filter((board) => board.material === 'Sheet') : null
   const hardwoodBoards = boards ? boards.filter((board) => board.material === 'Hardwood') : null
   const dimensionalBoards = boards ? boards.filter((board) => board.material === 'Dimensional') : null
+
   return (
       <>
         <div>
           {uploadButton &&
             <>
-              <FileUploadDialog label='CSV' mediaType='boards' projectId={currentProject._id}/>
+              <FileUploadDialog label='CSV' mediaType='boards' projectId={currentProject._id} />
               <span className='button-box'>
                 <Button className='p-shadow-3' type="button" label="Re-Import" icon="pi pi-fw pi-plus" style={{marginBottom: '.5em'}} onClick={() => importBoards()} ></Button>
               </span>
